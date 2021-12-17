24cdffc37586a2ba6cc6e86d868490c4be36f177
Jordan <jtrask@gmail.com>
2021-12-17 06:16:57 -0500

    Update cache-purge-helper.php
    Added appropriate comments for plugin details to show in WordPress

--- cache-purge-helper.php
+++ cache-purge-helper.php
@@ -1,14 +1,32 @@
-<?
+<?php
+
+/**
+ * Plugin Name:       Cache Purge Helper
+ * Plugin URI:        https://wpinfo.net
+ * Description:       Adding additional hooks to trigger nginx-helper or lscache plugin purges
+ * Version:           0.1
+ * Author:            Paul Stoute, Jordan Trask
+ * Author URI:        https://wpinfo.net
+ * Text Domain:       cache-purge-helper
+ * Domain Path:       /languages
+ * Requires at least: 3.0
+ * Tested up to:      5.4
+ *
+ * @link              https://wpinfo.net
+ * @since             0.1
+ * @package           cache-purge-helper
+ */
 
 //* Nginx Helper Enhancement
 function nhpcau_upgrader_process_complete() {
-
+  
   global $nginx_purger;
 
   if(isset($nginx_purger))
   {
     $nginx_purger->purge_all();
   }
+  
 }
 // After plugins have been updated
 add_action( 'upgrader_process_complete', 'nhpcau_upgrader_process_complete', 10, 0 );
