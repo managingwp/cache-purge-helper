78b298be657bb57706cfd9f680c78367e3097ea9
Jordan <jtrask@gmail.com>
2021-12-20 11:18:28 -0500

    Update cache-purge-helper.php
    Only log when WP_DEBUG is set.

--- cache-purge-helper.php
+++ cache-purge-helper.php
@@ -60,7 +60,9 @@ function cache_purge_helper() {
 if ( ! function_exists('write_log')) {
   function write_log ( $log )  {
     if ( is_array( $log ) || is_object( $log ) ) {
-      error_log( print_r( $log, true ) );
+      if ( WP_DEBUG === true ) {
+        error_log( print_r( $log, true ) );
+      }
     } else {
       error_log( $log );
     }
